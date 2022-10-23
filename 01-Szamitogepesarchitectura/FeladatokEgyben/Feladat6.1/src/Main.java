f421bd60ba382ff41bce4c5e030d865300a7b597
kolczegon <kolczegon94@gmail.com>
2022-10-23 12:42:43 +0300

    Feladat6.2

--- 01-Szamitogepesarchitectura/FeladatokEgyben/Feladat6.1/src/Main.java
+++ 01-Szamitogepesarchitectura/FeladatokEgyben/Feladat6.1/src/Main.java
@@ -4,5 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello world!");
+        System.out.println("Adj meg egy karakterlancot:");
+        String karakterlanc = sc.nextLine();
+        int hossz = karakterlanc.length();
+        String forditott = "";
+
+        for (int i = hossz-1; i >=0 ; --i) {
+           forditott = forditott + karakterlanc.charAt(i);
+
+           if (karakterlanc.equals(forditott))
+            {
+                System.out.println("Palindrom");
+
+            } else{
+                System.out.println("Nem palindrome");
+            }
+        }
+
     }
 }
