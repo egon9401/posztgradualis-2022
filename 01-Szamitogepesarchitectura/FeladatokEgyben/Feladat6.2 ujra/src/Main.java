72a031da14db15eb4967993bc53b0716a7a9531f
kolczegon <kolczegon94@gmail.com>
2022-10-30 15:30:29 +0200

    Feladat6.2 ujra

---
+++ 01-Szamitogepesarchitectura/FeladatokEgyben/Feladat6.2 ujra/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //2. Kérjen be egy karakterláncot. Majd ahány karakter van ebben (pl. alma esetén 4, kiskutya esetén
+        //8), annyiszor írja ki az első karaktert (pl. alma esetén aaaa, kiskutya esetén kkkkkkkk).
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adj meg egy karakterlancot");
+        String karakterlanc = sc.nextLine();
+
+        for (int i = 0; i < karakterlanc.length(); i++) {
+            System.out.println(karakterlanc.charAt(0));
+
+        }
+    }
+}
